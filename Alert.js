619df4344a307a194c8ffde68cef358d18f2238b
Emanuel Joness <itzemanuelj@gmail.com>
2022-08-07 12:18:39 -0400

    Alerts Code

---
+++ Alert.js
@@ -0,0 +1,24 @@
+
+//What is the keyword that displays a message box?
+alert
+//Type the character that ends every statement.
+;
+/* Type the 2 characters that are missing in this statement.
+alert"You're learning JavaScript!";*/
+alert("You're learing Javascript!")
+//A __________ ends in a semicolon.
+//A statement ends in a semicolon.
+alert("Hi there salior!");
+alert("Thanks for everything!");
+alert("BOO!");
+alert("You Won!");
+alert("Hi!");
+//What are the 5 punctuation characters of an alert statement, in order?
+("");
+//Code an alert statement displaying any message you like.
+alert("Hello World!");
+/*The code displays a box with a message in it. Fill in the blank.
+______("You won!");*/
+alert("You Won!");
+//Code an alert statement that displays your full name.
+alert("Justn Emanuel");
